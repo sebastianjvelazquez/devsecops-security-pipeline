1cc1f93aecd626b41f79397b1d9198bf68196cb7
Sebastian Velazquez <81114894+sebastianjvelazquez@users.noreply.github.com>
2025-10-24 17:59:30 -0400

    Added app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+
+def is_insecure_configuration(config):
+    password = "SuperSecretPassword123"
+    print("Hello from the security pipeline!")
+
+if __name__ == "__main__":
+    is_insecure_configuration()
